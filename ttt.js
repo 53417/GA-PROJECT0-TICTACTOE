43bcd720a81e4314ce26dcf31b88e5cd671143fc
53417 <seanimal@Live.com.au>
2019-09-18 23:19:23 +1000

    update 2019-09-18

--- ttt.js
+++ ttt.js
@@ -2,7 +2,7 @@ let boardData = [];
 let column = 0;
 let row = 0;
 let winCondition = 0;
-let turn = 1
+let turn = 0;
 let turnCount = 0;
 let p1Score = 0;
 let p2Score = 0;
@@ -11,30 +11,6 @@ let p1IconColor = "select-black";
 let p2IconPic = "fa-circle";
 let p2IconColor = "select-black";
 
-//generator
-function gameStart() {
-	let columns = document.getElementById('input-columns').value;
-	let rows = document.getElementById('input-rows').value;
-	let winConditions = document.getElementById('input-wincond').value;
-	var arr = [];
-	var count = 0;
-	column = columns;
-    row = rows;
-    winCondition = winConditions;
-
-  for (var i = 0; i < rows; i++) {
-    arr[i] = [];
-    for (var y = 0; y < columns; y++) {
-      count = count + 1;
-      arr[i].push(count);
-      boardData[boardData.length] = {ID: count, RID: i + 1, CID: y + 1, PLAYER: "no-input"}
-    }
-    //console.log(arr[i])
-  }
-  return 
-};
-
-//straight up
 function vertUp(cellid) {
 	if(boardData[cellid].RID < winCondition) {
 		return false
@@ -47,7 +23,6 @@ function vertUp(cellid) {
 	return true;
 };
 
-//straight left
 function vertLeft(cellid) {
 	if(boardData[cellid].CID < winCondition) {
 		return false
@@ -60,7 +35,6 @@ function vertLeft(cellid) {
 	return true;
 };
 
-//straight down
 function vertDown(cellid) {
 	if(boardData[cellid].RID > row - winCondition + 1) {
 		return false
@@ -73,7 +47,6 @@ function vertDown(cellid) {
 	return true;
 };
 
-//straight right
 function vertRight(cellid) {
 	if(boardData[cellid].CID > column - winCondition + 1) {
 		return false
@@ -86,7 +59,6 @@ function vertRight(cellid) {
 	return true;
 };
 
-// hoz top right
 function hozTopRight(cellid) {
 	if(boardData[cellid].RID < winCondition || 
 		boardData[cellid].CID > column - winCondition + 1) {
@@ -100,7 +72,6 @@ function hozTopRight(cellid) {
 	return true;
 };
 
-//hoz bottom right
 function hozBotRight(cellid) {
 	if(boardData[cellid].RID > row - winCondition + 1 || 
 		boardData[cellid].CID > column - winCondition + 1) {
@@ -114,7 +85,6 @@ function hozBotRight(cellid) {
 	return true;
 };
 
-//hoz bottom left
 function hozBotLeft(cellid) {
 	if(boardData[cellid].RID > row - winCondition + 1 || 
 		boardData[cellid].CID < winCondition) {
@@ -128,7 +98,6 @@ function hozBotLeft(cellid) {
 	return true;
 };
 
-//hoz top left
 function hozTopLeft(cellid) {
 	if(boardData[cellid].RID < winCondition || 
 		boardData[cellid].CID < winCondition) {
@@ -158,17 +127,37 @@ function winCheck(cid) {
 	return false
 };
 
-function gameStartHtml() {
-    let count = 1;
-    let gx = 1;
-    let gy = 1;
+function gameStart() {
+	//VAR CLEANSE
+	boardData = [];
+	turn = 1;
+	turnCount = 0;
 	document.getElementById("game-board").innerHTML = "";
+
+	//VAR RENEW
+	var arr = [];
+	var count = 0;
+
+	//TABLE DATA GENERATOR
+	for (var i = 0; i < row; i++) {
+		arr[i] = [];
+		for (var y = 0; y < column; y++) {
+			count = count + 1;
+			arr[i].push(count);
+			boardData[boardData.length] = {ID: count, RID: i + 1, CID: y + 1, PLAYER: "no-input"}
+		}
+	};
+
+	// HTML TABLE GENERATOR
+    let gx = 1;
+	let gy = 1;
+	let gCount = 1;
 	let tempTable = '<table class="game-board">';
     for(let y = 0; y < row; y++) {
 		tempTable += `<tr id="row${gy}">`;
         for (var x = 0; x < column; x++) {
-			tempTable += `<td id="cell${count}" style="width:${100/column}%;height:${100/row}%" class="game-cell"><\/td>`;
-            count = count + 1;
+			tempTable += `<td id="cell${gCount}" style="width:${100/column}%;height:${100/row}%"><\/td>`;
+            gCount = gCount + 1;
 		};
 		tempTable += '<\/tr>';
         gy = gy + 1;
@@ -176,75 +165,106 @@ function gameStartHtml() {
 	tempTable += '<\/table>';
 	document.getElementById('game-board').innerHTML = tempTable;
 	$(".game-board-box").css("height", $(".game-board-box").width());
+	
+
+	// CLICK GENERATOR
+	for(var i = 1; i < boardData.length + 1; i++) {
+		$('#cell' + i).click( cellClick(i) );
+	};
+
+	updateDisplay();
+
 };
 
 function cellClick(i){
 	return function(){
-
 		if(boardData[i-1].PLAYER !== "no-input") {
 			return
 		};
-
 		turnCount = turnCount + 1;
 		if(turnCount > boardData.length - 1) {
+			clearDisplay();
 			$("#draw-page").slideDown().delay(1200).slideUp();
-			gameStartFull();
+			gameStart();
 			turnCount = 0;
-		};
-
-		let tCell = `cell${i}`;
-		if(turn === 1) {
-			//document.getElementById('cell'+i).innerHTML = "X";
-			boardData[i-1].PLAYER = "X";
 			updateDisplay();
+			return;
+		};
+		if(turn === 1) {
+			boardData[i-1].PLAYER = "X";
 			if(winCheck(i - 1) === true) {
 				p1Score = p1Score + 1;
-				updateDisplay();
 				clearDisplay();
 				$("#p1-winner-page").slideDown().delay(1200).slideUp();
-				gameStartFull();
+				gameStart();
 				turnCount = 0;
 			};
 			turn = 2;
+			updateDisplay();
 			return;
 		};
 		if (turn === 2) {
-			//document.getElementById('cell'+i).innerHTML = "O";
 			boardData[i-1].PLAYER = "O";
 			updateDisplay();
 			if(winCheck(i - 1) === true) {
 				p2Score = p2Score + 1;
-				updateDisplay();
 				clearDisplay();
 				$("#p2-winner-page").slideDown().delay(1200).slideUp();
-				gameStartFull();
+				gameStart();
 				turnCount = 0;
 			};
 			turn = 1;
+			updateDisplay();
 			return;
 		}
 	}
 };
 
 function updateDisplay() {
-	//document.getElementById("p1-score").innerHTML = p1Score;
-	//document.getElementById("p2-score").innerHTML = p2Score;
-	for(let i = 0; i < boardData.length; i++) {
-		if(boardData[i].PLAYER === "X") {
-			document.getElementById('cell'+(i+1)).innerHTML = `<i class="fas ${p1IconPic} ${p1IconColor}"></i>`;
-		}
-		if(boardData[i].PLAYER === "O") {
-			document.getElementById('cell'+(i+1)).innerHTML = `<i class="fas ${p2IconPic} ${p2IconColor}"></i>`;
-		}
+	if(column >= row) {
+		for(let i = 0; i < boardData.length; i++) {
+			if(boardData[i].PLAYER === "X") {
+				document.getElementById('cell'+(i+1)).innerHTML = `<i class="fas ${p1IconPic} ${p1IconColor}" style="font-size:${25/column}vmin;width:${220/column}%;height:${220/row}%"></i>`;
+			}
+			if(boardData[i].PLAYER === "O") {
+				document.getElementById('cell'+(i+1)).innerHTML = `<i class="fas ${p2IconPic} ${p2IconColor}" style="font-size:${25/column}vmin;width:${220/column}%;height:${220/row}%"></i>`;
+			}
+		};
 	};
-	let scores = `
-		<a id="p1-score" class="player-icons">${p1Score}</a>
-		<a id="p1-icon" class="player-icons fas ${p1IconPic} ${p1IconColor}"></a>
-		<a class="player-icons">|</a>
-		<a id="p2-icon" class="player-icons fas ${p2IconPic} ${p2IconColor}"></a>
-		<a id="p2-score" class="player-icons">${p2Score}</a>`;
-	document.getElementById("player-scores").innerHTML = scores;
-
+	if(column < row) {
+		for(let i = 0; i < boardData.length; i++) {
+			if(boardData[i].PLAYER === "X") {
+				document.getElementById('cell'+(i+1)).innerHTML = `<i class="fas ${p1IconPic} ${p1IconColor}" style="font-size:${25/row}vmin;width:${220/column}%;height:${220/row}%"></i>`;
+			}
+			if(boardData[i].PLAYER === "O") {
+				document.getElementById('cell'+(i+1)).innerHTML = `<i class="fas ${p2IconPic} ${p2IconColor}" style="font-size:${25/row}vmin;width:${220/column}%;height:${220/row}%"></i>`;
+			}
+		};
+	};
+	if(turn === 2) {
+		return document.getElementById("player-scores").innerHTML =
+			`<a id="p1-score" class="player-icons">${p1Score}</a>
+			<a id="p1-icon" class="player-icons fas ${p1IconPic} ${p1IconColor}"></a>
+			<a class="player-icons">|</a>
+			<a id="p2-icon" class="player-icons fas ${p2IconPic} ${p2IconColor}"></a>
+			<a id="p2-score" class="player-icons" style="background-color:#C8C8C8; border-radius:25px">${p2Score}</a>`;
+	}
+	if(turn === 1) {
+		return document.getElementById("player-scores").innerHTML = 
+			`<a id="p1-score" class="player-icons" style="background-color:#C8C8C8; border-radius:25px">${p1Score}</a>
+			<a id="p1-icon" class="player-icons fas ${p1IconPic} ${p1IconColor}"></a>
+			<a class="player-icons">|</a>
+			<a id="p2-icon" class="player-icons fas ${p2IconPic} ${p2IconColor}"></a>
+			<a id="p2-score" class="player-icons">${p2Score}</a>`;
+	}
+	else {
+		return document.getElementById("player-scores").innerHTML =
+			`<a id="p1-score" class="player-icons">${p1Score}</a>
+			<a id="p1-icon" class="player-icons fas ${p1IconPic} ${p1IconColor}"></a>
+			<a class="player-icons">|</a>
+			<a id="p2-icon" class="player-icons fas ${p2IconPic} ${p2IconColor}"></a>
+			<a id="p2-score" class="player-icons">${p2Score}</a>`;
+	}
 };
 
 function clearDisplay() {
@@ -254,81 +274,129 @@ function clearDisplay() {
 	$("#p1-winner-page").slideUp();
 	$("#p2-winner-page").slideUp();
 	$("#draw-page").slideUp();
+	$("#error-on-start").slideUp();
+	$("#splash-page").slideUp();
 };
 
-function gameStartFull() {
-	boardData = [];
-	document.getElementById("game-board").innerHTML = "";
-	gameStart();
-	gameStartHtml();
-	for(var i = 1; i < boardData.length + 1; i++) {
-		$('#cell' + i).click( cellClick(i) );
-	};
-};
-
-$(document).ready(function(){  
-	$("#set-p1-button").click(function(){  
-		clearDisplay();
-		$("#set-p1-icon-page").slideDown();
-	});
-
-	$("#set-p2-button").click(function(){  
-		clearDisplay();
-		$("#set-p2-icon-page").slideDown();
-	});
-
-	$("#settings-button").click(function(){  
+$(document).ready(function(){
+	$("#splash-button").click(function(){  
 		clearDisplay();
 		$("#settings-page").slideDown();
 	});
+	
+	$("#settings-button").click(function(){  
+		$("#set-p1-icon-page").slideUp();
+		$("#set-p2-icon-page").slideUp();
+		$("#p1-winner-page").slideUp();
+		$("#p2-winner-page").slideUp();
+		$("#draw-page").slideUp();
+		$("#error-on-start").slideUp();
+		$("#splash-page").slideUp();
+		$("#settings-page").slideToggle();
+	});
 
-	$("#gameplay-button").click(function(){  
+	$("#set-p1-button").click(function(){  
+		$("#settings-page").slideUp();
+		$("#set-p2-icon-page").slideUp();
+		$("#p1-winner-page").slideUp();
+		$("#p2-winner-page").slideUp();
+		$("#draw-page").slideUp();
+		$("#error-on-start").slideUp();
+		$("#splash-page").slideUp();
+		$("#set-p1-icon-page").slideToggle();
+	});
+
+	$("#set-p2-button").click(function(){  
+		$("#settings-page").slideUp();
+		$("#set-p1-icon-page").slideUp();
+		$("#p1-winner-page").slideUp();
+		$("#p2-winner-page").slideUp();
+		$("#draw-page").slideUp();
+		$("#error-on-start").slideUp();
+		$("#splash-page").slideUp();
+		$("#set-p2-icon-page").slideToggle();
+	});
+
+	$("#home-button").click(function(){  
 		clearDisplay();
+		$("#splash-page").slideDown();
 	});
 
 	$(".selColorBox1").on('click', function(e){
-		// console.log($(this).children().children().closest('i'))
-		//console.log($(e.target).closest('i').prevObject[0].parentNode.classList[3]);
 		p1IconColor = $(e.target).closest('i').prevObject[0].parentNode.classList[3];
 		updateDisplay();
 	});
 
 	$(".selColorBox2").on('click', function(e){
-		//console.log($(e.target).closest('i').prevObject[0].parentNode.classList[3]);
 		p2IconColor = $(e.target).closest('i').prevObject[0].parentNode.classList[3];
 		updateDisplay();
 	});
 
 	$(".selIconBox1").on('click', function(e){
-		//console.log($(e.target).closest('i').prevObject[0].parentNode.classList[1]);
 		p1IconPic = $(e.target).closest('i').prevObject[0].parentNode.classList[1];
 		updateDisplay();
 	});
 
 	$(".selIconBox2").on('click', function(e){
-		//console.log($(e.target).closest('i').prevObject[0].parentNode.classList[1]);
 		p2IconPic = $(e.target).closest('i').prevObject[0].parentNode.classList[1];
 		updateDisplay();
 	});
 
 	$("#start-game").click(function(){
+		column = parseInt(document.getElementById('input-columns').value);
+		row = parseInt(document.getElementById('input-rows').value);
+		winCondition = parseInt(document.getElementById('input-wincond').value);
+		if(column < 0 || row < 0 || winCondition < 0) {
+			$("#error-on-start").html("<h2>Variables must be greater than 0</h2>");
+			$("#error-on-start").slideDown().delay(1200).slideUp();
+			return
+		};
+		if(winCondition > column && winCondition > row) {
+			$("#error-on-start").html("<h2>Win condition not possible</h2>");
+			$("#error-on-start").slideDown().delay(1200).slideUp();
+			return
+		};
 		clearDisplay();
-		gameStartFull();
+		gameStart();
+	});
+
+	$("#3x3Game").click(function(){
+		column = 3;
+		row = 3;
+		winCondition = 3;
+		clearDisplay();
+		gameStart();
+	});
+
+	$("#7x7Game").click(function(){
+		column = 7;
+		row = 7;
+		winCondition = 4;
+		clearDisplay();
+		gameStart();
+	});
+
+	$("#10x10Game").click(function(){
+		column = 10;
+		row = 10;
+		winCondition = 5;
+		clearDisplay();
+		gameStart();
 	});
 
 	$("#reset").click(function(){
-		console.clear();
+		document.getElementById("game-board").innerHTML = "";
 		clearDisplay();
 		boardData = [];
 		column = 0;
 		row = 0;
 		winCondition = 0;
-		turn = 1
+		turn = 1;
 		turnCount = 0;
 		p1Score = 0;
 		p2Score = 0;
-		updateDisplay;
+		updateDisplay();
+		$("#settings-page").slideDown();
 	});
 
 });
-//end doc ready functions
